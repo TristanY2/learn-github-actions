0cf2ae515e8c03d48f7145dfe6696b27316102be
TristanY <jmtristanyael@gmail.com>
2026-07-01 17:43:46 +0000

    t7

---
+++ feo.py
@@ -0,0 +1,6 @@
+import sys
+
+def sumar_numeros ( a , b ) :  # Error 2: Espacios horribles antes y después de los paréntesis
+      return a+b  # Error 3: Falta espacio alrededor del operador '+'
+
+print( sumar_numeros( 2, 2 ) )
